a6dfaba0bf457d7a7997a8b83bd2a8ba60f2a089
cihy999 <cihy999@gmail.com>
2024-09-28 15:55:56 +0800

    feat: 新增Log分類

---
+++ Plugins/CommonUIExtension/Source/CommonUIExtension/Private/CommonExtensionLogs.cpp
@@ -0,0 +1,6 @@
+// Copyright (C) Cindy Chen. All Rights Reserved.
+
+// Self
+#include "CommonExtensionLogs.h"
+
+DEFINE_LOG_CATEGORY(CommonWidgetEx);
---
+++ Plugins/CommonUIExtension/Source/CommonUIExtension/Public/CommonExtensionLogs.h
@@ -0,0 +1,5 @@
+// Copyright (C) Cindy Chen. All Rights Reserved.
+
+#pragma once
+
+DECLARE_LOG_CATEGORY_EXTERN(CommonWidgetEx, Log, All);
